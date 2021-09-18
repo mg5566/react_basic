2ce9308f0f7f55338310f1730dd29806430ef88a
강명구 <mg5566@hanmail.net>
2021-09-18 20:44:16 +0900

    update bcrypt

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.post('/register', (req, res) => {
 	}
 	*/
 	const user = new User(req.body)
+
 	user.save((err, userInfo) => {
 		if (err) return res.json({ success: false, err })
 		return res.status(200).json({ success: true })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
 	name: {
@@ -26,6 +28,30 @@ const userSchema = mongoose.Schema({
 	},
 	tokenExp: {
 		type: Number,
+	}
+})
+
+// 유저모델에 유저정보를 저장하기 전에 함수를 동작시킨다는 의미입니다.
+userSchema.pre('save', function (next) {
+	// 비밀번호를 암호화합니다.
+
+	// 모델로 생성된 schema 를 가져옵니다.
+	var user = this;
+
+	// 비밀번호만 변경될때, 동작하도록 합니다.
+	if (user.isModified('password')) {
+		// salt 를 생성
+		bcrypt.genSalt(saltRounds, function (err, salt) {
+			if (err) return next(err);
+			// schema 에서 password 를 가져옵니다.
+			bcrypt.hash(user.password, salt, function (err, hash) {
+				if (err) return next(err);
+				// Store hash in your password DB.
+				// hash 된 비밀번호로 저장합니다.
+				user.password = hash;
+				next();
+			});
+		});
 	}
 })
 

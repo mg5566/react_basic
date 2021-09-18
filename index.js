71a5c03da18b8223c0742b11add18e229a19a560
강명구 <mg5566@hanmail.net>
2021-09-18 19:01:53 +0900

    add nodemon and add bodyparser and add post router

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const port = 3000
 
 const mongoose = require("mongoose")
 
+const bodyParser = require('body-parser')
+// User model 을 import 합니다.
+const { User } = require("./models/User")
+
+// bodyParser 옵션 설정
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
+
 mongoose.connect("mongodb+srv://test:test@fuckingdb.rufm6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 	.then(() => {
 		console.log('MongoDB Connected...')
@@ -14,6 +24,23 @@ mongoose.connect("mongodb+srv://test:test@fuckingdb.rufm6.mongodb.net/myFirstDat
 
 app.get('/', (req, res) => {
 	res.send('Hello World!')
+})
+
+// post method
+app.post('/register', (req, res) => {
+	// 회원가입할때 필요한 정보들을 client 에서 가져오면 DB에 추가하기
+	// req.body 에 아래와 같은 형식으로 읽을 수 있도록 위에서 옵션을 추가했습니다.
+	/* bodyparser 좋네요
+	{
+	  id: "test"
+	  pw: "test"
+	}
+	*/
+	const user = new User(req.body)
+	user.save((err, userInfo) => {
+		if (err) return res.json({ success: false, err })
+		return res.status(200).json({ success: true })
+	})
 })
 
 app.listen(port, () => {
